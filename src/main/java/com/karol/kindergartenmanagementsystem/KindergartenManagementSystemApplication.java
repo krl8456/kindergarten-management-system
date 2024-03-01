a9b674007b8ef11c1b2c548fbc09ae2e6894de66
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-01 19:10:25 +0100

    feat: add initial files

---
+++ src/main/java/com/karol/kindergartenmanagementsystem/KindergartenManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.karol.kindergartenmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karol/kindergartenmanagementsystem/KindergartenManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karol.kindergartenmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergartenManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
