ab1e50aa8211bc5b46a4803d7abfd4f72237f55c
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-08 20:23:40 +0100

    feat: add security configuration for enabling login and stateless authentication

---
+++ src/main/java/com/karol/kindergartenmanagementsystem/config/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.karol.kindergartenmanagementsystem.config;
+
+import com.karol.kindergartenmanagementsystem.filter.JwtAuthenticationFilter;
+import com.karol.kindergartenmanagementsystem.service.UserAccountDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private final UserAccountDetailsService userAccountDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfiguration(UserAccountDetailsService userAccountDetailsService,
+                                 JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userAccountDetailsService = userAccountDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                request -> request.requestMatchers("/api/login/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                ).userDetailsService(userAccountDetailsService)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+        return configuration.getAuthenticationManager();
+    }
+}
