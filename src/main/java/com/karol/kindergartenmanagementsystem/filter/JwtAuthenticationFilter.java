0072095816c3af54df7bf04779fd0cd4178d2768
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-08 19:55:32 +0100

    feat: add JWTAuthenticationFilter for token validation and user authentication

---
+++ src/main/java/com/karol/kindergartenmanagementsystem/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.karol.kindergartenmanagementsystem.filter;
+
+import com.karol.kindergartenmanagementsystem.service.JwtService;
+import com.karol.kindergartenmanagementsystem.service.UserAccountDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserAccountDetailsService userAccountDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService,
+                                   UserAccountDetailsService userAccountDetailsService) {
+        this.jwtService = jwtService;
+        this.userAccountDetailsService = userAccountDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractEmail(token);
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userAccountDetailsService.loadUserByUsername(email);
+            if (jwtService.isValid(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
