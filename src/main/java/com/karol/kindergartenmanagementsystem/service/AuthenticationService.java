f03614898a700d4dd1c5f2ceee3e78b1eba74393
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-08 21:14:20 +0100

    feat: add AuthenticationService containing user authentication logic

---
+++ src/main/java/com/karol/kindergartenmanagementsystem/service/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.karol.kindergartenmanagementsystem.service;
+
+import com.karol.kindergartenmanagementsystem.model.AuthenticationResponse;
+import com.karol.kindergartenmanagementsystem.model.User;
+import com.karol.kindergartenmanagementsystem.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository,
+                                 JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Account with this email not found"));
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+}
