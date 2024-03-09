f35b0728a442bcddfc278d670a46996bdb18fd3c
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-09 18:46:09 +0100

    feat: add saving token to database when user is authenticated

--- src/main/java/com/karol/kindergartenmanagementsystem/service/AuthenticationService.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/service/AuthenticationService.java
@@ -1,7 +1,9 @@
 package com.karol.kindergartenmanagementsystem.service;
 
 import com.karol.kindergartenmanagementsystem.model.AuthenticationResponse;
+import com.karol.kindergartenmanagementsystem.model.Token;
 import com.karol.kindergartenmanagementsystem.model.User;
+import com.karol.kindergartenmanagementsystem.repository.TokenRepository;
 import com.karol.kindergartenmanagementsystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public AuthenticationResponse authenticate(User request) {
         authenticationManager.authenticate(
@@ -26,8 +29,18 @@ public class AuthenticationService {
 
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("Account with this email not found"));
+
         String token = jwtService.generateToken(user);
+        saveUserToken(token, user);
 
         return new AuthenticationResponse(token);
+    }
+
+    private void saveUserToken(String token, User user) {
+        Token tokenWithStatus = new Token();
+        tokenWithStatus.setToken(token);
+        tokenWithStatus.setLoggedOut(false);
+        tokenWithStatus.setUser(user);
+        tokenRepository.save(tokenWithStatus);
     }
 }
