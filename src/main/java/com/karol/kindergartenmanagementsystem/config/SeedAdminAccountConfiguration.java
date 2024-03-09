2012317120f84f5a4881d46ebe5dffa7a621f43e
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-09 15:41:04 +0100

    feat: add configuration for seeding admin account

---
+++ src/main/java/com/karol/kindergartenmanagementsystem/config/SeedAdminAccountConfiguration.java
@@ -0,0 +1,41 @@
+package com.karol.kindergartenmanagementsystem.config;
+
+import com.karol.kindergartenmanagementsystem.model.Role;
+import com.karol.kindergartenmanagementsystem.model.User;
+import com.karol.kindergartenmanagementsystem.repository.UserRepository;
+import com.karol.kindergartenmanagementsystem.service.UserAccountDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SeedAdminAccountConfiguration implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserAccountDetailsService userAccountDetailsService;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.count() == 0) {
+            User admin = User
+                    .builder()
+                    .firstName("admin")
+                    .lastName("admin")
+                    .phoneNumber("123456789")
+                    .email("admin@admin.com")
+                    .password(passwordEncoder.encode(adminPassword))
+                    .role(Role.ADMIN)
+                    .build();
+            userAccountDetailsService.save(admin);
+            log.debug("created ADMIN user - {}", admin);
+        }
+    }
+}
