789ae7b11382adb0c3ef59fe287a4a1305250f36
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-09 16:58:08 +0100

    refactor: remove unused imports

--- src/main/java/com/karol/kindergartenmanagementsystem/service/AuthenticationService.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/service/AuthenticationService.java
@@ -6,7 +6,6 @@ import com.karol.kindergartenmanagementsystem.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
