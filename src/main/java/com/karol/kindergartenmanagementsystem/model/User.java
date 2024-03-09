aef09ca851354cf9df74cb1b87164b4189774ceb
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-09 15:46:51 +0100

    feat: add timestamps to User model

--- src/main/java/com/karol/kindergartenmanagementsystem/model/User.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/model/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -29,7 +30,8 @@ public class User implements UserDetails {
     private String password;
     @Enumerated(value = EnumType.STRING)
     private Role role;
-
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
