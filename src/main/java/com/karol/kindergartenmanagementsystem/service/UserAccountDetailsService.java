9ca9e6e04d9dd565523ce5b0370993426d2a3bed
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-09 16:56:44 +0100

    feat: add method for saving users to database

--- src/main/java/com/karol/kindergartenmanagementsystem/service/UserAccountDetailsService.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/service/UserAccountDetailsService.java
@@ -1,10 +1,13 @@
 package com.karol.kindergartenmanagementsystem.service;
 
+import com.karol.kindergartenmanagementsystem.model.User;
 import com.karol.kindergartenmanagementsystem.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserAccountDetailsService implements UserDetailsService {
@@ -18,4 +21,13 @@ public class UserAccountDetailsService implements UserDetailsService {
         return userRepository.findByEmail(email)
                         .orElseThrow(() -> new UsernameNotFoundException("Account with this email not found"));
     }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setCreatedAt(LocalDateTime.now());
+        }
+        user.setUpdatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
 }
