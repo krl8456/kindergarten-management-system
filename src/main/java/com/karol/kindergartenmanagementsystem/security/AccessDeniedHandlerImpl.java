49a327a9ab7bbc759827e7e3200893babc541a84
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-26 23:26:40 +0100

    feat: add logging for access denied exceptions to track unauthorized access attempts

--- src/main/java/com/karol/kindergartenmanagementsystem/security/AccessDeniedHandlerImpl.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/security/AccessDeniedHandlerImpl.java
@@ -2,15 +2,18 @@ package com.karol.kindergartenmanagementsystem.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException){
+        log.warn("Access denied for user: {} trying to access: {}", request.getRemoteUser(), request.getRequestURI());
         response.setStatus(HttpStatus.FORBIDDEN.value());
     }
 }
