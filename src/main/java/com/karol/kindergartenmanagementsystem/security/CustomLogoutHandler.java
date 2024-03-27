51c755247c4e0b3f040a645dd0375d303130ca55
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-27 14:49:59 +0100

    refactor: change manually checking for null to using Optional with logging

--- src/main/java/com/karol/kindergartenmanagementsystem/security/CustomLogoutHandler.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/security/CustomLogoutHandler.java
@@ -1,10 +1,10 @@
 package com.karol.kindergartenmanagementsystem.security;
 
-import com.karol.kindergartenmanagementsystem.model.Token;
 import com.karol.kindergartenmanagementsystem.repository.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
@@ -14,6 +14,7 @@ import static com.karol.kindergartenmanagementsystem.http.AuthorizationHeaderPro
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomLogoutHandler implements LogoutHandler {
     private final TokenRepository tokenRepository;
     @Override
@@ -22,14 +23,16 @@ public class CustomLogoutHandler implements LogoutHandler {
                        Authentication authentication) {
         String authHeader = request.getHeader(AUTHORIZATION_HEADER);
         if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
+            log.info("No authorization header found or it does not start with the expected prefix.");
             return;
         }
 
         String token = authHeader.substring(TOKEN_PREFIX.length());
-        Token storedToken = tokenRepository.findByToken(token).orElse(null);
-        if (token != null) {
+        tokenRepository.findByToken(token).ifPresentOrElse(storedToken -> {
             storedToken.setLoggedOut(true);
             tokenRepository.save(storedToken);
-        }
+        }, () -> {
+            log.warn("No token found for logout: {}", token);
+        });
     }
 }
