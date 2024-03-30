6f47485a007bd3b1499731bd8cd4f057ac73bf30
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-30 23:40:49 +0100

    refactor: move details about admin account to properties file

--- src/main/java/com/karol/kindergartenmanagementsystem/init/AdminAccountSeeder.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/init/AdminAccountSeeder.java
@@ -8,30 +8,40 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@PropertySource("classpath:admin.properties")
 public class AdminAccountSeeder implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserAccountDetailsService userAccountDetailsService;
 
-    @Value("${admin.password}")
-    private String adminPassword;
+    @Value("${firstname}")
+    private String firstName;
+    @Value("${lastname}")
+    private String lastName;
+    @Value("${phoneNumber}")
+    private String phoneNumber;
+    @Value("${email}")
+    private String email;
+    @Value("${password}")
+    private String password;
 
     @Override
     public void run(String... args) {
         if (userRepository.count() == 0) {
             User admin = User
                     .builder()
-                    .firstName("admin")
-                    .lastName("admin")
-                    .phoneNumber("123456789")
-                    .email("admin@admin.com")
-                    .password(passwordEncoder.encode(adminPassword))
+                    .firstName(firstName)
+                    .lastName(lastName)
+                    .phoneNumber(phoneNumber)
+                    .email(email)
+                    .password(passwordEncoder.encode(password))
                     .role(Role.ADMIN)
                     .build();
             userAccountDetailsService.save(admin);
