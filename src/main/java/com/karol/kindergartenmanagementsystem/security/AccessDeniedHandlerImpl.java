2d00dac44f66ff3a996692852c5acaac73291ab9
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-03-30 23:48:25 +0100

    refactor: change hardcoded values to constants

--- src/main/java/com/karol/kindergartenmanagementsystem/security/AccessDeniedHandlerImpl.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/security/AccessDeniedHandlerImpl.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,13 @@ import java.time.LocalDateTime;
 @Component
 @Slf4j
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+    private final String responseMessage = "{\"message\":\"Access denied\"}";
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         log.info("Access denied for user: {} trying to access: {} at {}", request.getRemoteUser(), request.getRequestURI(), LocalDateTime.now());
         response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.setContentType("application/json");
-        response.getWriter().write("{\"message\":\"Access denied\"}");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(responseMessage);
     }
 }
--- src/main/java/com/karol/kindergartenmanagementsystem/security/AuthenticationEntryPointImpl.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/security/AuthenticationEntryPointImpl.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,13 @@ import java.time.LocalDateTime;
 @Component
 @Slf4j
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    private final String responseMessage = "{\"message\":\"Authentication failed\"}";
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException{
         log.info("Attempted unauthorized access: User '{}' attempted to access '{}' at {}", request.getRemoteUser(), request.getRequestURI(), LocalDateTime.now());
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setContentType("application/json");
-        response.getWriter().write("{\"message\":\"Authentication failed\"}");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(responseMessage);
     }
 }
