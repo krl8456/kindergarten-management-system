2988c8f2bea4c02f9090bc6edea1335791a7a3ea
krl8456 <83764437+krl8456@users.noreply.github.com>
2024-04-04 17:17:36 +0200

    feat: disable possibility to instantiate an utility class

--- src/main/java/com/karol/kindergartenmanagementsystem/http/AuthorizationHeaderProperties.java
+++ src/main/java/com/karol/kindergartenmanagementsystem/http/AuthorizationHeaderProperties.java
@@ -3,4 +3,8 @@ package com.karol.kindergartenmanagementsystem.http;
 public class AuthorizationHeaderProperties {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
+
+    private AuthorizationHeaderProperties() {
+        throw new AssertionError();
+    }
 }
